9a0960f4ad75e8aa1d971a57fc317a303bd940d0
parkg2 <50623955+parkg2@users.noreply.github.com>
2025-01-20 15:12:17 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'gensim==3.8.3',
-        'numpy==2.2.2',
-        'openai==1.59.8',
-        'pandas==2.2.3',
-        'pyLDAvis==3.4.1',
-        'scikit_learn==1.6.1',
-        'setuptools==69.5.1'
+        'pyLDAvis==3.4.1'
      ],
     python_requires='>=3.9',  # Specify Python version compatibility if necessary
 )
