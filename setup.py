b2ee64b14f22ade4da984ee1b988b6007009e077
parkg2 <50623955+parkg2@users.noreply.github.com>
2025-01-21 22:41:59 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='lda_eval_lib',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'gensim==3.8.3',
         'pyLDAvis==3.4.1'
      ],
-    python_requires='>=3.8',  # Specify Python version compatibility if necessary
+    python_requires='>=3.9',  # Specify Python version compatibility if necessary
 )
