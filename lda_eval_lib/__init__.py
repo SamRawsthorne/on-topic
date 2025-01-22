32b71380c7b56b375d58a63d74fddb17e2d41c4f
parkg2 <50623955+parkg2@users.noreply.github.com>
2025-01-22 18:27:12 +0000

    Update __init__.py

--- lda_eval_lib/__init__.py
+++ lda_eval_lib/__init__.py
@@ -4,3 +4,4 @@ from .gpt_labelling import labelling
 from .metrics import diversity, granularity, IDXtopics2keep
 from .custom_pyvis import visualize_topics
 from .util import create_corpus, save_lda_run, load_lda_run
+from .gensim_ldamallet import malletmodel2ldamodel, LdaMallet
