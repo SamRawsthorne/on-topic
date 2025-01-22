cd479e30a82b93743937e1bb78baf5cae06b0e08
parkg2 <50623955+parkg2@users.noreply.github.com>
2025-01-22 18:11:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     install_requires=[
         'openai==1.59.8',
         'gensim==3.8.3',
-        'pyLDAvis==3.3.1',
+        'pyLDAvis==3.4.1',
         'scikit-learn==1.5.1',
         'scipy==1.10.1'
      ],
-    python_requires='>=3.7, <3.9',  # Specify Python version compatibility if necessary
+    python_requires='>=3.9, <3.10',  # Specify Python version compatibility if necessary
     include_package_data=True,
     package_data={
         "": ["test/*.pkl"],  # Include all .pkl files in the 'test' folder
